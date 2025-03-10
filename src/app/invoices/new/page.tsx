1431bb2489a754ac313b4328cfa7f48a7b583764
Ahuv04 <ganeshkarakhilesh@gmail.com>
2025-03-10 11:02:23 +0530

    Integrated XATA using Drizzle

--- src/app/invoices/new/page.tsx
+++ src/app/invoices/new/page.tsx
@@ -1,14 +1,48 @@
-import {Button } from "@/components/ui/button"
-import Link from 'next/link'
-import { Badge } from "@/components/ui/badge"
-import { CirclePlus } from "lucide-react"
+import {sql} from "drizzle-orm";
+import { db } from "@/db";
 
-export default function Home() {
+import { Label } from "@/components/ui/label"
+import { Input } from "@/components/ui/input"
+import { Textarea } from "@/components/ui/textarea"
+import { Button } from "@/components/ui/button";
+
+export default async function Home() {
+  const results = db.execute(sql `SELECT current_database()`);
   return (
-      <main className="max-w-5xl mx-auto flex flex-col justify-center gap-6 text-center my-12">
+      <main className="max-w-5xl mx-auto flex flex-col justify-center gap-6 my-12">
         <div className="flex justify-between">
             <h1 className="text-3xl font-semibold"> Dashboard</h1>
         </div>
+
+        <form className="grid gap-4 max-w-xs">
+          <div>
+            <Label htmlFor="name" className="block mb-2 font-semibold text-sm">Billing Name</Label>
+            <Input id="name" name="name" type="text"></Input>
+            </div>
+
+            { JSON.stringify(results)}
+            <div>
+            <Label htmlFor="email" className="block mb-2 font-semibold text-sm">Billing Email</Label>
+            <Input id="email" name="email" type="email"></Input>
+            </div>
+
+            <div>
+            <Label htmlFor="value" className="block mb-2 font-semibold text-sm">Value</Label>
+            <Input id="value" name="value" type="text"></Input>
+            </div>
+
+            <div>
+            <Label htmlFor="description" className="block mb-2 font-semibold text-sm">Description</Label>
+            <Textarea id="description" name="description"></Textarea>
+            </div>
+
+            <div>
+              <Button className="w-full font-semibold">
+                Submit
+              </Button>
+            </div>
+
+        </form>
       </main>
   );
 }
---
+++ src/db/index.ts
@@ -0,0 +1,27 @@
+import { drizzle } from "drizzle-orm/node-postgres";
+import { eq } from "drizzle-orm";
+import { pgTable, text } from "drizzle-orm/pg-core";
+
+//with client you can set up a connection(drizzle instance) but with pool you can create pool of connections for a single client
+
+import { Pool } from "pg";
+
+const pool = new Pool({ connectionString: process.env.DATABASE_URL, max: 20 });
+export const db = drizzle(pool);
+
+/*Client connection sample code
+import { Client } from "pg";
+const client = new Client({ connectionString: process.env.DATABASE_URL });
+await client.connect();
+const db = drizzle(client);
+const tableName = pgTable("tableName", {
+  xata_id: text("xata_id").primaryKey(),
+});
+
+const record = await db
+  .select()
+  .from(tableName)
+  .where(eq(tableName.xata_id, "rec_xyz"))
+  .execute();
+console.log(record);
+*/
